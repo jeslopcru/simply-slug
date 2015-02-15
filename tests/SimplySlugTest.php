730d9286b5ff505253ed12e919a97a6cc77f55c7
jeslopcru <jeslopcru@gmail.com>
2015-02-15 12:12:25 +0100

    configuration project finished

---
+++ tests/SimplySlugTest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace SimplySlug\Tests;
+
+class SimplySlugTest extends \PHPUnit_Framework_TestCase
+{
+    public function testNothing()
+    {
+        $this->assertTrue(true);
+    }
+}
